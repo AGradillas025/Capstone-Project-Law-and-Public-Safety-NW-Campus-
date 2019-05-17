defdfe476fc6ce819ee98b83f7dc06297be24679
Emmanuel Cortes <ecortes016@west-mec.org>
2019-05-17 14:56:46 -0700

    Form Validation
    Added form validation to the contact page

--- script.js
+++ script.js
@@ -5,6 +5,44 @@
     File: script.js
 */
 
-function formValidation {
+window.onload = function () {
+    document.getElementById("submit").onclick = submit;
+    document.getElementById("name").oninput = nameValidation;
+    document.getElementById("email").oninput = emailValidation;
+    document.getElementById("commBox").onkeydown = questionValidation;
+}
 
+function submit() {
+    questionValidation();
+    nameValidation();
+    emailValidation();
+}
+
+function nameValidation() {
+    var fullName = document.getElementById("name");
+    if (fullName.validity.valueMissing) {
+        fullName.setCustomValidity("Please enter your full name.");
+    } else {
+        fullName.setCustomValidity("");
+    }
+}
+
+function emailValidation() {
+    var email = document.getElementById("email");
+
+    if (email.validity.valueMissing) {
+        email.setCustomValidity("Please enter your email address.");
+    } else {
+        email.setCustomValidity("");
+    }
+}
+
+function questionValidation() {
+    var comments = document.getElementById("commBox");
+
+    if (comments.validity.valueMissing) {
+        comments.setCustomValidity("Please let us know your questions!");
+    } else {
+        comments.setCustomValidity("");
+    }
 }
